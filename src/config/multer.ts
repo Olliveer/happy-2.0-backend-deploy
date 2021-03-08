2538ccc81bf50703585e4939d4544fb1fd8d106e
olliveer <j.oliveira90@live.com>
2021-03-08 13:18:34 -0300

    tsconfig

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -5,43 +5,43 @@ import multerS3 from 'multer-s3';
 import path from 'path';
 
 const s3 = new aws.S3();
-const storageTypes = {
-  local: multer.diskStorage({
-    destination: (request, file, callback) => {
-      callback(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
-    },
-    filename: (request, file, callback) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) {
-          return callback(err, '');
-        }
+const storageLocal = multer.diskStorage({
+  destination: (request, file, callback) => {
+    callback(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
+  },
+  filename: (request, file, callback) => {
+    crypto.randomBytes(16, (err, hash) => {
+      if (err) {
+        return callback(err, '');
+      }
 
-        const originalName = file.originalname.split(' ').join('-')
-        const fileName = `${hash.toString('hex')}-${originalName}`;
-        callback(null, fileName);
-      });
-    },
-  }),
-  s3: multerS3({
-    s3: new aws.S3(),
-    bucket: process.env.STORAGE_BUCKET as string,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read',
-    key: (req, file, callback) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) callback(err);
+      const originalName = file.originalname.split(' ').join('-')
+      const fileName = `${hash.toString('hex')}-${originalName}`;
+      callback(null, fileName);
+    });
+  },
+});
 
-        const originalName = file.originalname.split(' ').join('-')
-        const fileName = `${hash.toString('hex')}-${originalName}`;
-        callback(null, fileName);
-      })
-    },
-  })
-}
+const storageS3 = multerS3({
+  s3: new aws.S3(),
+  bucket: process.env.STORAGE_BUCKET as string,
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  acl: 'public-read',
+  key: (req, file, callback) => {
+    crypto.randomBytes(16, (err, hash) => {
+      if (err) callback(err);
+
+      const originalName = file.originalname.split(' ').join('-')
+      const fileName = `${hash.toString('hex')}-${originalName}`;
+      callback(null, fileName);
+    })
+  },
+});
+
 
 const config: multer.Options = {
   dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: storageTypes['s3'],
+  storage: storageS3,
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
